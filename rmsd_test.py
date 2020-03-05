2b5dd75c63e2db9f8150dcf69be37a0629ff14eb
Miguel Luengo Perez <44966857+mluengo@users.noreply.github.com>
2020-03-05 18:26:42 +0100

    Function to calculate rmsd

---
+++ rmsd_test.py
@@ -0,0 +1,71 @@
+from __future__ import print_function
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import rmsd
+
+from tensorsHelper import pre_process_pdb_into_vectors, normalize_vector_of_int
+
+def rotation_matrix(sigma):
+    """
+    https://en.wikipedia.org/wiki/Rotation_matrix
+    """
+
+    radians = sigma * np.pi / 180.0
+
+    #r11 = 1
+    #r12 = 0
+    #r13 = 0
+
+    #r21 = 0
+    #r22 = np.cos(radians)
+    #r23 = -np.sin(radians)
+
+    #r31 = 0
+    #r32 = np.sin(radians)
+    #r33 = np.cos(radians)
+
+    r11 = np.cos(radians)
+    r12 = -np.sin(radians)
+    r13 = 0
+
+    r21 = np.sin(radians)
+    r22 = np.cos(radians)
+    r23 = 0
+
+    r31 = 0
+    r32 = 0
+    r33 = 1
+
+    R = np.array([[r11, r12,r13], [r21, r22,r23], [r31, r32,r33]])
+
+    return R
+
+
+pdb_vectors = pre_process_pdb_into_vectors()
+
+vector1 = np.asarray(pdb_vectors[0])
+vector2 = np.asarray(pdb_vectors[1])
+
+A = vector1[:,6:9]
+B = vector2[:,6:9]
+
+
+# Rotate
+B = np.dot(B, rotation_matrix(90))
+
+print("Normal RMSD", rmsd.rmsd(A, B))
+
+
+# Manipulate
+A -= rmsd.centroid(A)
+B -= rmsd.centroid(B)
+
+print("Translated RMSD", rmsd.rmsd(A, B))
+
+
+U = rmsd.kabsch(A, B)
+A = np.dot(A, U)
+
+print("Rotated RMSD", rmsd.rmsd(A, B))
